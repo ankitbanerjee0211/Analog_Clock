b2ce97cc5d6b1262a8ef83380749ebbe3c5c791c
ankitbanerjee0211 <ankitbanerjee0211@gmail.com>
2021-07-26 13:12:10 +0530

    Updated content

--- js/script.js
+++ js/script.js
@@ -9,14 +9,36 @@ setInterval(() => {
     hour.style.transform = `rotate(${hRotation}deg)`;
     minute.style.transform = `rotate(${mRotation}deg)`;
     second.style.transform = `rotate(${sRotation}deg)`;
-    if (h%3 == 0) {
-        document.getElementById("clockContainer").style.background = "url('https://www.pikpng.com/pngl/m/25-252413_clocks-clipart-rectangle-square-clock-without-hands-png.png') no-repeat";
+    if (h%4 == 0) {
+        document.getElementById("clockContainer").style.background = "url('img/clock5.png') no-repeat";
         document.getElementById("clockContainer").style.backgroundSize = "100%";
+        document.getElementById("hour").style.left = "50.40%";
+        document.getElementById("hour").style.top = "31%";
+        document.getElementById("hour").style.height = "18%";
+        document.getElementById("hour").style.backgroundColor = "#A1A1A1";
+
+
+        document.getElementById("minute").style.left = "50.80%";
+        document.getElementById("minute").style.top = "26%";
+        document.getElementById("minute").style.height = "23%";
+        document.getElementById("minute").style.backgroundColor = "#696960";
+
+
+
+        document.getElementById("second").style.left = "51.15%";
+        document.getElementById("second").style.top = "17%";
+        document.getElementById("second").style.height = "32%";
+        document.getElementById("second").style.backgroundColor = "#10110D";
+
     }
-    else if (h%3 == 1) {
+    else if (h%4 == 1) {
         document.getElementById("clockContainer").style.background = "url('img/clock1.png') no-repeat";
         document.getElementById("clockContainer").style.backgroundSize = "100%";
         document.getElementById("clockContainer").style.filter = "invert(0)";
+    }
+    else if (h%4 == 2) {
+        document.getElementById("clockContainer").style.background = "url('img/clock4.png') no-repeat";
+        document.getElementById("clockContainer").style.backgroundSize = "100%";
     }
     else {
         document.getElementById("clockContainer").style.background = "url('img/clock3.png') no-repeat";
