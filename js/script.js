42e9effd3094c47ed5e2e9c59385f2275b7ebb7d
ankitbanerjee0211 <ankitbanerjee0211@gmail.com>
2021-07-26 12:12:55 +0530

    Update

--- js/script.js
+++ js/script.js
@@ -9,12 +9,17 @@ setInterval(() => {
     hour.style.transform = `rotate(${hRotation}deg)`;
     minute.style.transform = `rotate(${mRotation}deg)`;
     second.style.transform = `rotate(${sRotation}deg)`;
-    if (h%2 == 0) {
-        document.getElementById("clockContainer").style.background = "url('img/clock3.png') no-repeat";
+    if (h%3 == 0) {
+        document.getElementById("clockContainer").style.background = "url('https://www.pikpng.com/pngl/m/25-252413_clocks-clipart-rectangle-square-clock-without-hands-png.png') no-repeat";
         document.getElementById("clockContainer").style.backgroundSize = "100%";
     }
-    else {
+    else if (h%3 == 1) {
         document.getElementById("clockContainer").style.background = "url('img/clock1.png') no-repeat";
+        document.getElementById("clockContainer").style.backgroundSize = "100%";
+        document.getElementById("clockContainer").style.filter = "invert(0)";
+    }
+    else {
+        document.getElementById("clockContainer").style.background = "url('img/clock3.png') no-repeat";
         document.getElementById("clockContainer").style.backgroundSize = "100%";
     }
 }, 1000);
